0f53b6a1a244ce70a81066159fea98c3f7c92509
OswaldoPineda <carlos-1502010@hotmail.com>
2019-03-30 12:33:33 -0600

    [Oswaldo] - Add gem colorize. Added a gem colorize to implement colors in terminal to give a better UX.

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -3,6 +3,7 @@ require_relative "./TicTacToe/game"
 #require_relative "./TicTacToe/helper"
 require_relative "./TicTacToe/player"
 require_relative "./TicTacToe/version"
+require "colorize"
 
 module TicTacToe
     
